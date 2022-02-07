5328ae1c22991d239a05358b7785b7135f36eef6
LS <1948275556@qq.com>
2022-02-07 11:36:16 +0800

    整理OSHI文件

--- src/test/java/com/ls/completed/OSHITest.java
+++ src/test/java/com/ls/completed/OSHITest.java
@@ -1,42 +1,20 @@
 package com.ls.completed;
 
-import com.ls.completed.util.ServerOSUtil;
+import cn.hutool.core.lang.Console;
+import cn.hutool.system.oshi.CpuInfo;
+import cn.hutool.system.oshi.OshiUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import oshi.SystemInfo;
-import oshi.software.os.OperatingSystem;
 
 
 @SpringBootTest
 public class OSHITest {
     @Test
     void oshiSysInfTest(){
-        SystemInfo systemInfo = new SystemInfo();
-        OperatingSystem operatingSystem = systemInfo.getOperatingSystem();
-        System.out.println(systemInfo.getOperatingSystem());
-        System.out.println(systemInfo.getHardware());
-        System.out.println(operatingSystem.getFamily());
-        System.out.println(operatingSystem.getVersionInfo());
+        long total = OshiUtil.getMemory().getTotal();
+        System.out.println(total);
 
-        System.out.println(operatingSystem.getFileSystem().getFileStores());
-        System.out.println(operatingSystem.getFileSystem().getMaxFileDescriptors());
-        System.out.println(operatingSystem.getFileSystem().getOpenFileDescriptors());
-
-        System.out.println(operatingSystem.getNetworkParams().getIpv4DefaultGateway());
-        System.out.println(operatingSystem.getNetworkParams().getIpv6DefaultGateway());
-
-        System.out.println(operatingSystem.getProcessCount());
-        System.out.println(operatingSystem.getThreadCount());
-
-        System.out.println(systemInfo.getHardware().getProcessor());
-        System.out.println(systemInfo.getHardware().getComputerSystem());
-        System.out.println(systemInfo.getHardware().getMemory());
-        System.out.println(systemInfo.getHardware().getDiskStores());
-        System.out.println(systemInfo.getHardware().getNetworkIFs());
-    }
-
-    @Test
-    void ServerOsUtilTest(){
-        System.out.println(ServerOSUtil.getInfo());
+        CpuInfo cpuInfo = OshiUtil.getCpuInfo();
+        Console.log(cpuInfo);
     }
 }
