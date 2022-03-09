7c3a3c4a91f40c2c1e81f7897bf82707e122a6be
LS <1948275556@qq.com>
2022-03-09 11:02:22 +0800

    更新生产环境配置文件及前端静态文件

--- src/main/java/com/ls/completed/CompletedApplication.java
+++ src/main/java/com/ls/completed/CompletedApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.ls.completed.mapper")
 public class CompletedApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CompletedApplication.class, args);
     }
-
 }
